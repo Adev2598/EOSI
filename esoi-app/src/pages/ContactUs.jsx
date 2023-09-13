2b87d07402cbc7be52589205e8fb14820a5a5225
Adev2598 <ammar.react.dev@outlook.com>
2023-09-13 15:06:26 +0400

    stage 7.1

--- esoi-app/src/pages/ContactUs.jsx
+++ esoi-app/src/pages/ContactUs.jsx
@@ -5,7 +5,7 @@ const ContactUs = () => {
   return (
     <>
         <div className='dark:bg-slate-800 pb-32 text-center'>
-            <h1 className='dark:text-white text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl sm:mx-14'> We're here to help</h1>
+            <h1 className='dark:text-white text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'> We're here to help</h1>
         </div>
         <ContactForm/>
     </>
