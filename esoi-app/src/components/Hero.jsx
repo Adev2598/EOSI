edf52822e55a6389f46288b518cd77c402e6b641
Adev2598 <ammar.react.dev@outlook.com>
2023-09-26 15:59:33 +0400

    stage 12

--- esoi-app/src/components/Hero.jsx
+++ esoi-app/src/components/Hero.jsx
@@ -1,24 +1,30 @@
 import React from 'react'
+import Element14Color from '../assets/eosi-elements/14.png'
+import studentFull from '../assets/images/studentFull.png'
+import '../Styles/animations.css'
 
 const Hero = () => {
 
   return (
     <>
     <div className="bg-white dark:bg-slate-800">
-            <div className="relative isolate px-6 pt-14 lg:px-8">
-        <div
+      
+      <div className='grid lg:grid-cols-2 sm:grid-cols-1 lg:px-24'>
+
+      <div className="relative isolate px-6 pt-14 lg:px-8 ">
+        {/* <div
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
           aria-hidden="true"
         >
             
-          {/* <div
+          <div
             className=" dark:hidden light:bg-white relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[20deg] bg-gradient-to-tr from-white to-[#7db8e4] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
                 'polygon(74.1% 44.1%, 100% 61.9%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
             }}
-          /> */}
-        </div>
+          />
+        </div> */}
         <div className="mx-auto max-w-2xl py-32 sm:py-8 lg:pt-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="dark:text-white relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 dark:ring-white ring-gray-900/10 dark:hover:ring-[#0a6aab] hover:ring-[#004169]/20">
@@ -50,20 +56,28 @@ const Hero = () => {
             </div>
           </div>
         </div>
-        <div
+        {/* <div
           className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
           aria-hidden="true"
         >
-          {/* <div
+          <div
             className="dark:hidden relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
             }}
-          /> */}
-        </div>
+          />
+        </div> */}
       </div>
-    </div>
+        <div>
+          <img src={studentFull} alt="student" className='lg:mt-32 sm:my-auto bounce-in'/>
+        </div>      
+        </div>
+        
+      <div>        
+      </div>
+      
+    </div> 
    
     </>
   )
--- esoi-app/src/pages/About.jsx
+++ esoi-app/src/pages/About.jsx
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import { Carousel, Button, message, Steps, theme } from 'antd';
 import '../Styles/animations.css'
 import Element14Color from '../assets/eosi-elements/14.png'
+import globe from '../assets/images/globe.png'
 
 const contentStyleSlider = {
   height: '250px',
@@ -11,7 +12,7 @@ const contentStyleSlider = {
 
 const steps = [
   {
-    title: 'First',
+    title: 'Register',
     content: 'First-content',
   },
   {
@@ -61,7 +62,7 @@ const About = () => {
             <div id="about" className="text-center px-4">
               <h1 className="dark:text-white text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
                 About
-                </h1>
+              </h1>
               <div className="dark:text-white mt-6 text-lg leading-8 text-gray-600 ">
                 The Energy Oman Student Initiative aims to provide a platform for students 
                 interested in the energy sector to gain knowledge,
@@ -69,7 +70,7 @@ const About = () => {
                 The program aims to foster learning, networking, and career development opportunities for students.            
               </div>
             </div>
-            <img src={Element14Color} className="scale-75 mx-auto" alt="student"/>
+            <img src={globe} className="mx-auto my-14" alt="globe"/>
           </div>
        
           <div id="programs" className='mx-auto max-w-2xl py-auto lg:py-0'>
