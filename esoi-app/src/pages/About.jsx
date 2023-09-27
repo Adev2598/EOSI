c3009e6e8e3d40959c5ee5103935d82f844ac3ed
Adev2598 <ammar.react.dev@outlook.com>
2023-09-27 14:34:48 +0400

    Update About.jsx

--- esoi-app/src/pages/About.jsx
+++ esoi-app/src/pages/About.jsx
@@ -1,58 +1,45 @@
 import React, {useState} from "react";
-import { Carousel, Button, message, Steps, theme } from 'antd';
+import { ArrowPathIcon, CloudArrowUpIcon, FingerPrintIcon, LockClosedIcon } from '@heroicons/react/24/outline'
 import '../Styles/animations.css'
 import Element14Color from '../assets/eosi-elements/14.png'
 import globe from '../assets/images/globe.png'
 
-const contentStyleSlider = {
-  height: '250px',
-  lineHeight: '250px',
-  textAlign: 'center',
-};
-
-const steps = [
+const features = [
   {
-    title: 'Register',
-    content: 'First-content',
+    name: 'Student Contributor Program',
+    description:
+      'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.',
+    icon: CloudArrowUpIcon,
   },
   {
-    title: 'Second',
-    content: 'Second-content',
+    name: 'Mentorship Program',
+    description:
+      'Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.',
+    icon: LockClosedIcon,
   },
   {
-    title: 'Last',
-    content: 'Last-content',
+    name: 'Energy Oman Student Awards',
+    description:
+      'Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.',
+    icon: ArrowPathIcon,
   },
-];
+  {
+    name: 'Industry Visits & Field Trips',
+    description:
+      'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
+    icon: FingerPrintIcon,
+  },
+  {
+    name: 'Visits & Field Trips',
+    description:
+      'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
+    icon: FingerPrintIcon,
+  },
+]
 
 const About = () => {
 
-    const { token } = theme.useToken();
-    const [current, setCurrent] = useState(0);
-    const next = () => {
-      setCurrent(current + 1);
-    };
-
-    const prev = () => {
-      setCurrent(current - 1);
-    };
-
-    const items = steps.map((item) => ({
-      key: item.title,
-      title: item.title,
-    }));
-
-    const contentStyle = {
-      lineHeight: '260px',
-      textAlign: 'center',
-      color: token.colorTextTertiary,
-      backgroundColor: token.colorFillAlter,
-      borderRadius: token.borderRadiusLG,
-      border: `1px dashed ${token.colorBorder}`,
-      marginTop: 16,
-    };
-
-  return (
+    return (
     <>
           {/*<div className='dark:bg-slate-800'>
               <img src={Element9White} />
@@ -74,13 +61,36 @@ const About = () => {
           </div>
        
           <div id="programs" className='mx-auto max-w-2xl py-auto lg:py-0'>
-            <div id="about" className="text-center px-4">
-              <h1 className="dark:text-white text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl pb-24">
-                Program Components
-                </h1>
-            </div>
+  
+       <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:text-center">
+          {/* <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2> */}
+          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
+            Program Components
+          </p>
+          {/* <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-white">
+            Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum
+            pulvinar et feugiat blandit at. In mi viverra elit nunc.
+          </p> */}
+        </div>
+        <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+          <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+            {features.map((feature) => (
+              <div key={feature.name} className="relative pl-16 ">
+                <dt className="text-base font-semibold leading-7 text-gray-900 dark:text-white">
+                  <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                    <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
+                  </div>
+                  {feature.name}
+                </dt>
+                <dd className="mt-2 text-base leading-7 text-gray-600">{feature.description}</dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </div>
 
-          <div className="pb-24">
+          {/* <div className="pb-24">
             <Carousel effect="fade" autoplay>
               <div className="text-xl text-white font-medium sm:px-8 flex bg-[#0a6aab] dark:bg-[#f5f5f5] dark:text-gray-900 rounded-lg">
                 <h1 style={contentStyleSlider}>Student Contributor Program</h1>
@@ -103,7 +113,7 @@ const About = () => {
                 More Info
               </a>
             </div>
-          </div>
+          </div> */}
 
           <div className='mx-auto max-w-2xl pt-48 pb-32 sm:py-48 lg:py-20'>
             <div id="about" className="text-center px-4">
@@ -113,36 +123,7 @@ const About = () => {
             </div>            
           </div>
 
-          <div className="bg-white p-8 rounded">
-          <Steps current={current} items={items} />
-      <div style={contentStyle}>{steps[current].content}</div>
-      <div
-        style={{
-          marginTop: 24,
-        }}
-      >
-        {current < steps.length - 1 && (
-          <Button className="bg-primary" onClick={() => next()}>
-            Next
-          </Button>
-        )}
-        {current === steps.length - 1 && (
-          <Button onClick={() => message.success('Processing complete!')}>
-            Done
-          </Button>
-        )}
-        {current > 0 && (
-          <Button
-            style={{
-              margin: '0 8px',
-            }}
-            onClick={() => prev()}
-          >
-            Previous
-          </Button>
-        )}
-      </div>
-          </div>
+
 
             </div>   
           </div>
