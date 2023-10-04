ed24c60b35ec4cdd544bd84651ddcd4acb7e4b55
Adev2598 <ammar.react.dev@outlook.com>
2023-10-04 09:17:41 +0400

    removed programs from About to new component

---
+++ esoi-app/src/components/Programs.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../Styles/programs.css'
+
+const Programs = () => {
+    
+  return (
+   
+        <div className='mx-auto max-w-2xl pt-48 pb-32 sm:py-48 lg:py-20 dark:bg-slate-800'>
+            <div id="about" className="text-center px-4">
+              <h1 className="dark:text-white text-4xl font-bold text-gray-900 sm:text-6xl">
+                Program Components
+              </h1>              
+            </div>            
+          
+     </div>
+  )
+}
+
+export default Programs
